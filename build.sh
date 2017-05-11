a50bea82ba3c805622c0f86312fb31e1879a9473
Danil P <slemonide@gmail.com>
2017-05-10 22:27:32 -0700

    Update build script. It can now build Windows executables.

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+function createLoveFile {
+    zip -9 -r game.love `dirname $0`
+}
+
+function buildWindowsExe {
+    wget https://bitbucket.org/rude/love/downloads/love-0.10.2-win32.zip
+    unzip love-0.10.2-win32.zip
+    cat love-0.10.2-win32/love.exe game.love > game.exe
+    cp love-0.10.2-win32/*dll .
+}
+
+createLoveFile
+buildWindowsExe
