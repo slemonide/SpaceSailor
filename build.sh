2c2046c339b10461a049c6f94d575980e4369428
Danil P <slemonide@gmail.com>
2017-05-11 22:39:08 -0700

    Make the build script compile a Windows executable.

--- build.sh
+++ build.sh
@@ -6,10 +6,12 @@ LOVE_VERSION="0.10.2"
 
 # Downloads required libraries
 function downloadLibraries {
-    mkdir "lib"
-    cd "lib"
-    wget "https://bitbucket.org/rude/love/downloads/love-${LOVE_VERSION}-win32.zip"
-    cd -
+    if [ ! -f "lib/love-${LOVE_VERSION}-win32.zip" ]; then
+        mkdir "lib"
+        cd "lib"
+        curl -LOk -# "https://bitbucket.org/rude/love/downloads/love-${LOVE_VERSION}-win32.zip"
+        cd -
+    fi
 }
 
 # Builds .love file
@@ -27,16 +29,8 @@ function createLoveFile {
 # Builds Windows .exe file
 function buildWindowsExe {
     unzip -o "lib/love-${LOVE_VERSION}-win32.zip" -d "target"
-    tmp="target/tmp/"
-    mkdir -p "${tmp}/${PROJECT_NAME}"
-    cat "target/love-${LOVE_VERSION}-win32/love.exe" "target/${PROJECT_NAME}.love"
-        > "$tmp/${PROJECT_NAME}/${PROJECT_NAME}.exe"
-    cp "target/love-${LOVE_VERSION}-win32/*dll" "target/love-${LOVE_VERSION}-win32/license*" "${tmp}/${PROJECT_NAME}"
-    cd "${tmp}"
-    zip -9 -r - "$PROJECT_NAME" > "${PROJECT_NAME}-win.zip"
-    cd -
-    cp "${tmp}/${PROJECT_NAME}-win.zip" "target/"
-    rm -r "${tmp}"
+    cat "target/love-${LOVE_VERSION}-win32/love.exe" "target/${PROJECT_NAME}.love" > "target/${PROJECT_NAME}.exe"
+    cp -r target/love-${LOVE_VERSION}-win32/*dll "target"
 }
 
 # Checks that all lua files have correct syntax
@@ -57,7 +51,10 @@ fi
 
 ### build
 if [ "$1" == "build" ]; then
-    downloadLibraries
+    echo "Building LOVE file..."
     createLoveFile
+    echo "Downloading libraries..."
+    downloadLibraries
+    echo "Building Windows executable..."
+    buildWindowsExe
 fi
-#buildWindowsExe
